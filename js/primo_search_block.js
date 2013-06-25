68877ce3882fa22b4a72db28c2206a3b0cbd7584
Steven Bassett <steven.j.bassett@gmail.com>
2013-06-25 11:52:01 -0400

    Adding the the finalized version of the form rendered through drupal

--- js/primo_search_block.js
+++ js/primo_search_block.js
@@ -1,20 +1,9 @@
-function searchPrimo() {
-  document.getElementById("primoQuery").value = "any,contains," + document.getElementById("primoQueryTemp").value;
-  document.forms["searchForm"].submit();
-}
-function searchPrimoEvent(event) {
-  if (event && event.which === 13) {
+function searchPrimo() { 
+  if(document.getElementById("primoQueryTemp").value.length > 0){
     document.getElementById("primoQuery").value = "any,contains," + document.getElementById("primoQueryTemp").value;
     document.forms["searchForm"].submit();
   }
   else{
-    return true;
-  }
-}
-function enterKey(evt) {
-  var evt = (evt) ? evt : event;
-  var charCode = (evt.which) ? evt.which : evt.keyCode;
-  if (charCode === 13) {
-    searchPrimo();
+    window.stop();
   }
 }
